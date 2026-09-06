920ed74a2b357266e81d8dbc185b75e13c74c6a1
Andrei Mago <alexandrei.mago1@gmail.com>
2026-09-06 23:03:53 +0800

    Up to this point i've added the Check winner function, adding the winning combinations and the logic of getting a win. i was thinking of an ai but maybe i'll do that after making the 2 player game work

--- draft.js
+++ draft.js
@@ -17,3 +17,53 @@ const board = Array.from({ length: 9 }, () => "");
      getBoard, placeMark
   };
 })();
+
+
+function Player(name, marker){
+  return { name, marker }
+}
+
+const GameController = (() => {
+  const player1 = Player("Player 1", "X");
+  const player2 = Player("Player 2", "O");
+  let currentPlayer = player1;
+
+  const switchPlayer = () => {
+    currentPlayer = currentPlayer === player1 ? player2 : player1;
+  };
+
+  const getActivePlayer = () => currentPlayer;
+
+  const playRound = (index) => {
+    Gameboard.placeMark(index, currentPlayer.marker)
+    console.log(Gameboard.getBoard());
+    if (checkWinner()){
+      console.log(`${currentPlayer.name} wins!`);
+    }
+    switchPlayer();
+  }
+
+  return { playRound, getActivePlayer }
+})();
+
+// CHECK WINNER FUNCTION
+
+const checkWinner = (()=> {
+  const winningCombinations = [
+    [0,1,2],[3,4,5],[6,7,8],
+    [0,3,6],[1,4,7],[2,5,8],
+    [2,4,6],[0,4,8]
+  ];
+  const board = Gameboard.getBoard();
+
+  for (const combo of winningCombinations){
+    const [a,b,c] = combo
+    if (board[a] !== "" && board[a] === board[b] && board[b] === board[c]){
+      console.log(`The Winner is ${board[a]}`)
+      return true
+    }
+  }
+  return false
+})
+
+
